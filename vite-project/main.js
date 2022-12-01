bfffa29693c20e3240617ce44e1ce999df50af76
navneetjain89 <navneetjain89@gmail.com>
2022-12-01 12:35:07 +0530

    updated HTML, CSS & JS

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,10 @@
 import './src/index.css'
+
+// Grab the button
+let menu = document.getElementById('menuButton')
+let menuItems = document.getElementById('menuItems')
+
+// Add Event Listner and Function
+menu.addEventListener('click', ()=>{
+ menuItems.classList.toggle('max-lg:hidden')   
+})
